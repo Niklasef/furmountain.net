5f19279e6b39966ee948d84ea016196875f3fd27
Niklas Furberg <niklas.furberg@gmail.com>
2024-12-22 21:48:10 +0100

    move dns to its own function

--- core.sh
+++ core.sh
@@ -3,33 +3,39 @@
 # Source the .profile to get environment variables
 source /home/niklas/.profile
 
-RESOURCE_GROUP="furmountain-net"
-ZONE_NAME="furmountain.net"
+# Function to update DNS records
+update_dns() {
+    RESOURCE_GROUP="furmountain-net"
+    ZONE_NAME="furmountain.net"
+    # Get the current public IP
+    CURRENT_IP=$(curl -s https://api.ipify.org)
 
-# Get the current public IP
-CURRENT_IP=$(curl -s https://api.ipify.org)
+    # Login to Azure
+    az login --service-principal --username "$AZURE_SVC_APP_ID" --password "$AZURE_SVC_PASSWORD" --tenant "$AZURE_TENANT_ID"
 
-# Login to Azure
-az login --service-principal --username $AZURE_SVC_APP_ID --password $AZURE_SVC_PASSWORD --tenant $AZURE_TENANT_ID
+    # Convert HOST_NAMES into an array
+    IFS=' ' read -r -a HOST_NAMES_ARRAY <<< "$HOST_NAMES"
 
-# Convert HOST_NAMES into an array
-IFS=' ' read -r -a HOST_NAMES_ARRAY <<< "$HOST_NAMES"
+    # Loop through each host name
+    for HOST_NAME in "${HOST_NAMES_ARRAY[@]}"; do
+        echo "Updating DNS for host: $HOST_NAME"
 
-# Loop through each host name
-for HOST_NAME in "${HOST_NAMES_ARRAY[@]}"; do
-    # Delete the existing A record set
-    az network dns record-set a delete \
-        --yes \
-        --resource-group $RESOURCE_GROUP \
-        --zone-name $ZONE_NAME \
-        --name $HOST_NAME \
-        --subscription $AZURE_SUBSCRIPTION_ID
+        # Delete the existing A record set
+        az network dns record-set a delete \
+            --yes \
+            --resource-group "$RESOURCE_GROUP" \
+            --zone-name "$ZONE_NAME" \
+            --name "$HOST_NAME" \
+            --subscription "$AZURE_SUBSCRIPTION_ID"
 
-    # Create a new A record set with the current IP
-    az network dns record-set a add-record \
-        --resource-group $RESOURCE_GROUP \
-        --zone-name $ZONE_NAME \
-        --record-set-name $HOST_NAME \
-        --ipv4-address $CURRENT_IP \
-        --subscription $AZURE_SUBSCRIPTION_ID
-done
+        # Create a new A record set with the current IP
+        az network dns record-set a add-record \
+            --resource-group "$RESOURCE_GROUP" \
+            --zone-name "$ZONE_NAME" \
+            --record-set-name "$HOST_NAME" \
+            --ipv4-address "$CURRENT_IP" \
+            --subscription "$AZURE_SUBSCRIPTION_ID"
+    done
+}
+
+update_dns
