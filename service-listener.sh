aefdb2000aef498cce064f1b41828b4b166b0c1e
Niklas Furberg <niklas.furberg@gmail.com>
2024-12-25 22:32:34 +0100

    general service listener, for both local and remote

---
+++ service-listener.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Validate input parameters
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <host> <service>"
+    exit 1
+fi
+
+# Input parameters
+HOST="$1"
+SERVICE="$2"
+
+# MQTT Parameters
+MQTT_BROKER="localhost"
+MQTT_TOPIC="furmountain/${HOST}/${SERVICE}"
+
+# InfluxDB Parameters
+INFLUXDB_HOST="localhost"
+INFLUXDB_PORT="8086"
+INFLUXDB_DATABASE="mydb"
+INFLUXDB_MEASUREMENT="${SERVICE}"  # Use service name as measurement
+
+# Function to store data in InfluxDB
+function store_in_influxdb {
+    local value=$1
+
+    # Construct the data line for InfluxDB
+    local data="${INFLUXDB_MEASUREMENT},service=${SERVICE},instance=${HOST} value=${value}"
+
+    # Use curl to send the data to InfluxDB
+    curl -i -XPOST "http://${INFLUXDB_HOST}:${INFLUXDB_PORT}/write?db=${INFLUXDB_DATABASE}" \
+        --data-binary "${data}"
+}
+
+# Main loop to listen for MQTT messages and process them
+mosquitto_sub -h "${MQTT_BROKER}" -t "${MQTT_TOPIC}" | while read -r message; do
+    echo "Received ${SERVICE} data: ${message}"
+    store_in_influxdb "${message}"
+done
