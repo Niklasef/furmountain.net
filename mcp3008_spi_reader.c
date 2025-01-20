21406a775e78812b0802c0b6d96cfee60c2102ff
Niklas Furberg <niklas.furberg@gmail.com>
2025-01-20 19:49:43 +0100

    no sleep in c prog

--- mcp3008_spi_reader.c
+++ mcp3008_spi_reader.c
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
             clock_gettime(CLOCK_MONOTONIC, &start);
         }
 
-        usleep(delay);
+        // usleep(delay);
     }
 
     close(spi_fd);
